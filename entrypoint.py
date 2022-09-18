d4ef9629e989a93b401685430f4010302c6360e6
shikhar thapliyal <shikhar1707@gmail.com>
2022-09-18 17:47:24 +0530

    First commit

---
+++ entrypoint.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return jsonify({"message": "URL home"})
+
+
+@app.route("/name")
+def hello():
+    return jsonify({"message": "URL name"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
